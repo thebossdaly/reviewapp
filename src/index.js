24ccc495b50ae1502449fa8e7f2bb578bfca2167
thebossdaly <142459731+thebossdaly@users.noreply.github.com>
2023-11-04 22:53:06 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-let element = React.createElement('h1', {}, 'This is React!');
-ReactDOM.render(element, document.getElementById('app'));
